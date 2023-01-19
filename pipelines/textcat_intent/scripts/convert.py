e29d3fe4fba31c59f1f178bd7c89812f479aa140
pathbreak <5163416+pathbreak@users.noreply.github.com>
2023-01-19 20:10:34 +0530

    intent classification

---
+++ pipelines/textcat_intent/scripts/convert.py
@@ -0,0 +1,45 @@
+"""Convert textcat annotation from CSV to spaCy v3 .spacy format."""
+import warnings
+from pathlib import Path
+
+import csv
+import json
+
+import spacy
+from spacy.tokens import DocBin
+
+
+def main(input_path: Path, cats_json:Path, output_path: Path):
+    
+    
+    with open(cats_json, 'r') as f:
+        cats = json.load(f)
+        
+    one_hot_dicts = {}
+    for c in cats:
+        one_hot_dict = {t:(1 if t==c else 0) for t in cats }
+        one_hot_dicts[c] = one_hot_dict
+        
+
+    nlp = spacy.blank('en')
+    
+    db = DocBin()
+    
+    with open(input_path, 'r') as f:
+        reader = csv.reader(f)
+        
+        hdr = next(reader)
+        
+        for row in reader:
+            text = row[0]
+            cat = row[1]
+            
+            doc = nlp.make_doc(text)
+            doc.cats = one_hot_dict[cat]
+            db.add(doc)
+
+    db.to_disk(output_path)
+
+
+if __name__ == "__main__":
+    main()
